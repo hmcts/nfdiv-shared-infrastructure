024747996b1a39f52ae310609d6e5be567ee54f4
Linus Norton <linusnorton@gmail.com>
2021-07-21 10:13:02 +0100

    Add preview vault

--- main.tf
+++ main.tf
@@ -45,6 +45,32 @@ resource "azurerm_application_insights" "appinsights" {
     ]
   }
 }
+
+resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY_PREVIEW" {
+  name         = "AppInsightsInstrumentationKey-Preview"
+  value        = azurerm_application_insights.appinsights_preview.instrumentation_key
+  key_vault_id = module.key-vault.key_vault_id
+  count = var.env == "aat" ? 1 : 0
+}
+
+resource "azurerm_application_insights" "appinsights_preview" {
+  name                = "${var.product}-appinsights-preview"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+  count = var.env == "aat" ? 1 : 0
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance..
+      application_type,
+    ]
+  }
+}
+
 /*
 data "azurerm_key_vault_secret" "alerts_email" {
   name      = "alerts-email"
